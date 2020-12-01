fb176c9b7f4631d6a30092ee6d8389467d61e2e3
Yannick Ferire <yannick@bureau347.com>
2020-12-02 00:27:25 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,280 @@
+<template>
+  <!-- Base : 😁, Playing : 🤔, Loose : 🤯, Win : 🎉 -->
+  <h1>😁 Memoji</h1>
+  <p>A card matching game with emojis !</p>
+  <button v-if="newPlayer" @click="startGame" class="button">
+    <div class="button-icon">
+      <img class="icon-play" src="/images/play.svg" alt="Play Icon" /><img
+        class="icon-ghost"
+        src="/images/cute-ghost.svg"
+        alt="Cute Ghost"
+      />
+    </div>
+    Start Game
+  </button>
+  <button v-else @click="restartGame" class="button">
+    <img src="/images/restart.svg" alt="Restart Icon" />Restart Game
+  </button>
+  <transition-group tag="section" class="game-board" name="shuffle-card">
+    <Card
+      v-for="card in cardList"
+      :key="`${card.value}-${card.variant}`"
+      :matched="card.matched"
+      :value="card.value"
+      :visible="card.visible"
+      :position="card.position"
+      @select-card="flipCard"
+    />
+  </transition-group>
+  <h2 class="status">{{ status }}</h2>
+</template>
+
+<script>
+import { ref, watch } from 'vue'
+import createDeck from './features/createDeck'
+import createGame from './features/createGame'
+import { launchConfetti } from './utilities/confetti'
+import Card from './components/Card'
+import lvl1 from './data/lvl1.json'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  },
+  setup() {
+    const { cardList } = createDeck(lvl1)
+    const {
+      newPlayer,
+      startGame,
+      restartGame,
+      matchesFound,
+      status
+    } = createGame(cardList)
+    const userSelection = ref([])
+
+    const flipCard = payload => {
+      cardList.value[payload.position].visible = true
+
+      if (userSelection.value[0]) {
+        if (
+          userSelection.value[0].position === payload.position &&
+          userSelection.value[0].faceValue === payload.faceValue
+        ) {
+          return
+        } else {
+          userSelection.value[1] = payload
+        }
+      } else {
+        userSelection.value[0] = payload
+      }
+    }
+
+    watch(matchesFound, currentValue => {
+      if (currentValue === 8) {
+        launchConfetti()
+      }
+    })
+
+    watch(
+      userSelection,
+      currentValue => {
+        if (currentValue.length === 2) {
+          const cardOne = currentValue[0]
+          const cardTwo = currentValue[1]
+
+          if (cardOne.faceValue === cardTwo.faceValue) {
+            cardList.value[cardOne.position].matched = true
+            cardList.value[cardTwo.position].matched = true
+          } else {
+            setTimeout(() => {
+              cardList.value[cardOne.position].visible = false
+              cardList.value[cardTwo.position].visible = false
+            }, 2000)
+          }
+
+          userSelection.value.length = 0
+        }
+      },
+      { deep: true }
+    )
+
+    return {
+      cardList,
+      flipCard,
+      userSelection,
+      status,
+      restartGame,
+      startGame,
+      newPlayer
+    }
+  }
+}
+</script>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+h1 {
+  margin-top: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding: 60px 0;
+}
+
+.description {
+  font-family: 'Titillium Web', sans-serif;
+}
+
+.description p {
+  margin: 0;
+  font-size: 1rem;
+}
+
+.description p:last-child {
+  margin-top: 10px;
+  margin-bottom: 30px;
+  font-size: 0.9rem;
+}
+
+.status {
+  font-family: 'Titillium Web', sans-serif;
+  font-size: 18px;
+  text-transform: uppercase;
+}
+
+.button {
+  background-color: #e78805;
+  color: white;
+  padding: 8px 16px 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto 30px;
+  font-weight: bold;
+  font-family: 'Titillium Web', sans-serif;
+  font-weight: bold;
+  font-size: 1rem;
+  border: 0;
+  border-radius: 10px;
+  transition: 0.2s all ease-in;
+  border: 2px solid #e78805;
+}
+
+.button:hover {
+  border: 2px solid #e78805;
+  background-color: transparent;
+}
+
+.button:hover .icon-ghost {
+  opacity: 1;
+}
+
+.button:hover .icon-play {
+  opacity: 0;
+}
+
+.button img {
+  padding-right: 12px;
+  transition: 0.2s opacity ease-in;
+}
+
+.button .icon-ghost {
+  position: absolute;
+  left: -6px;
+  opacity: 0;
+}
+
+.button .icon-play {
+  position: absolute;
+  left: -6px;
+  opacity: 1;
+}
+
+.button-icon {
+  position: relative;
+  width: 24px;
+  height: 24px;
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: repeat(4, 60px);
+  grid-template-rows: repeat(4, 60px);
+  grid-column-gap: 12px;
+  grid-row-gap: 12px;
+  justify-content: center;
+}
+
+@media screen and (min-width: 500px) {
+  .game-board {
+    grid-template-columns: repeat(4, 90px);
+    grid-template-rows: repeat(4, 90px);
+  }
+}
+
+@media screen and (min-width: 600px) {
+  .game-board {
+    grid-template-columns: repeat(4, 120px);
+    grid-template-rows: repeat(4, 120px);
+  }
+}
+
+.sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  border: 0;
+}
+
+.title {
+  padding-bottom: 30px;
+}
+
+.shuffle-card-move {
+  transition: transform 0.8s ease-in;
+}
+
+.powered-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 15px;
+}
+
+.powered-link {
+  padding: 0 5px;
+}
+
+.powered-link:first-child,
+.powered-link:last-child {
+  padding-right: 0;
+}
+
+.powered-logo {
+  width: 18px;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="card" :class="flippedStyles" @click="selectCard">
+    <div class="card-face is-front">
+      {{value}}
+      <img v-if="matched" src="/images/checkmark.svg" class="icon-checkmark" />
+    </div>
+    <div class="card-face is-back"></div>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue'
+
+export default {
+  props: {
+    matched: {
+      type: Boolean,
+      default: false
+    },
+    position: {
+      type: Number,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props, context) {
+    const flippedStyles = computed(() => {
+      if (props.visible) {
+        return 'is-flipped'
+      }
+    })
+
+    const selectCard = () => {
+      context.emit('select-card', {
+        position: props.position,
+        faceValue: props.value
+      })
+    }
+
+    return {
+      flippedStyles,
+      selectCard
+    }
+  }
+}
+</script>
+
+<style>
+.card {
+  position: relative;
+  transition: 0.5s transform ease-in;
+  transform-style: preserve-3d;
+}
+
+.card.is-flipped {
+  transform: rotateY(180deg);
+}
+
+.card-face {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  border-radius: 10px;
+  font-size: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  backface-visibility: hidden;
+}
+
+.card-face.is-front {
+  background: #ccc;
+  color: white;
+  transform: rotateY(180deg);
+}
+
+.card-face.is-back {
+  background: #ccc;
+  color: white;
+}
+
+.card-image {
+  max-width: 100%;
+}
+
+.icon-checkmark {
+  position: absolute;
+  right: 5px;
+  bottom: 5px;
+}
+</style>
